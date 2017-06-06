41f060e3fa14b380828486969e0b17d882300b45
Michael Regert <michael_regert@dell.com>
2017-06-06 13:21:31 -0500

    Initial Tests

---
+++ Microservices/scripts/SMI/handlers/FirmwareUpdateMicroservice.py
@@ -0,0 +1,54 @@
+'''
+Created on June 5, 2017
+@author: Michael Regert
+'''
+
+import json
+import os
+import sys
+
+run_dir=os.path.abspath(os.path.dirname(__file__))
+current_dir = os.getcwd()
+os.chdir(run_dir)
+sys.path.insert(0,os.path.abspath('../utility'))
+
+from UtilBase import Utility
+
+class FirmwareUpdateHandler(Utility):
+    def __init__(self):
+        global logger
+        logger = self.getLoggerInstance()
+    
+    def getVersion(self):
+        logger.info("FirmwareUpdateHandler: getVersion()")
+        requestData, url = self.getRequestData("getVersion")
+        headers = {'Content-Type': 'application/json'}
+        action="GET"
+        result = self.getResponse(action, url, requestData, headers)
+        logger.info("Results from FirmwareUpdateHandler: getVersion(): \n" + result.text)
+        return result
+
+    def getCatalog(self, parameter_list):
+        logger.info("FirmwareUpdateHandler: getCatalog()")
+        requestData, url = self.getRequestData("getCatalog")
+        headers = {'Content-Type': 'application/json'}
+        action="GET"
+        for index, param in enumerate(parameter_list):
+            sub = "{" + str(index) + "}"
+            url = url.replace(sub, param)
+        result = self.getResponse(action, url, requestData, headers)
+        logger.info("Results from FirmwareUpdateHandler getCatalog(): \n" + result.text)
+        return result
+        
+
+    def getRequestData(self, task):
+        logger.info("FirmwareUpdateHandler: getRequestData()")
+        with open("../requestdata/firmwareUpdateRequestPayload.json") as data_file:
+            data = json.load(data_file)
+            requestData = data["services"][task]["data"]
+            url = self.__class__.host + data["services"][task]["url"]
+            return requestData, url
+        
+
+if __name__ == "__main__":  
+    test = FirmwareUpdateHandler()
---
+++ Microservices/scripts/SMI/tests/FirmwareUpdate.py
@@ -0,0 +1,66 @@
+'''
+Created on June 5, 2017
+@author: Michael Regert
+'''
+
+import json
+import unittest
+import sys
+import os
+import sys
+run_dir=os.path.abspath(os.path.dirname(__file__))
+current_dir = os.getcwd()
+os.chdir(run_dir)
+sys.path.insert(0,os.path.abspath('../utility'))
+sys.path.append(os.path.abspath('../handlers'))
+
+from UtilBase import Utility
+from FirmwareUpdateMicroservice import FirmwareUpdateHandler
+
+class FirmwareUpdateTest(unittest.TestCase):
+    global logger
+    logger = Utility().getLoggerInstance()
+
+    ########################################################################
+    # Test Get Version
+    def test001_GetVersion(self):
+        try:
+            response = FirmwareUpdateHandler().getVersion()
+            logger.info("FirmwareUpdateTest: test001_GetVersion(): Response Status Code: " + str(response.status_code))
+            self.assertEqual(response.status_code, 200, "Response code should equal 200")
+
+        except Exception as e1:
+            logger.error("FirmwareUpdateTest: test001_GetVersion():  Exception: " + str(e1))
+            raise e1
+
+    ########################################################################
+    # Test Get Catalog
+    def test002_GetCatalog(self):
+        try:
+            fileName  = 'Catalog.xml.gz'
+            fileUrl   = 'ftp.dell.com/catalog'
+            targetLoc = '/temp/'
+            response  = FirmwareUpdateHandler().getCatalog([fileName, fileUrl, targetLoc])
+            logger.info("FirmwareUpdateTest: test002_GetCatalog(): Response Status Code: " + str(response.status_code))           
+            logger.info("FirmwareUpdateTest: test002_GetCatalog(): Response Text: " + response.text)
+
+            # Check that the call returns a 200 (Success)
+            self.assertEqual(response.status_code, 200, "Response code should equal 200")
+
+            # Check that the file name returned is what we passed in
+            responseJson = json.loads(response.text)
+            self.assertGreater(responseJson["name"], fileName, "Returned filename should match what was passed in")
+
+
+        except Exception as e1:
+            logger.error("FirmwareUpdateTest: test002_GetCatalog():  Exception: " + str(e1))
+            raise e1
+
+if __name__=="__main__":
+    if len(sys.argv) > 1:
+        FirmwareUpdateHandler.host = sys.argv.pop()
+    else:
+        FirmwareUpdateHandler.host = "http://100.68.123.238:46010"
+#        FirmwareUpdateHandler.host = "http://localhost:46010"
+
+    unittest.main()
