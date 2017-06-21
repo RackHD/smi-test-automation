241e0ff0a1ed7e78bbdc620f1f5a5eda32cc1a62
Akash Kwatra <akashkw@gmail.com>
2017-06-21 15:37:30 -0500

    Add functionality to test runner
    Add support for modifier characters, host specification, and more configuration options. Clean and streamline code. Improve documentation. Add tester and test cases for parsing method.

--- Microservices/scripts/SMI/tests/run_tests.py
+++ Microservices/scripts/SMI/tests/run_tests.py
@@ -1,27 +1,45 @@
 # -*- coding: utf-8 -*-
 
+#   __      ___  ___ _  __ ___          ___ _           ___ ___  _
+#  (_  |\/|  | __ | |_ (_   | __ /\  | | | / \ |\/|  /\  |   |  / \ |\ |
+#  __) |  | _|_   | |_ __)  |   /--\ |_| | \_/ |  | /--\ |  _|_ \_/ | \|
+
 '''
 SMI-TEST-AUTOMATION
 ~~~~~~~~~~~~~~~~~~~
 
-Run tests on the specified microservices.
+Run specified microservice tests on specified host.
 Tests run in no particular order.
 
-Alias Reference
-DISC:1 CHIN:2 SVIN:3 PWTH:4 SCP:5 VID:6 VNW:7
+-- Structure --
+The default host is 'localhost'
+Each microservice is assigned a 1 character ID
+Each microservice ID is associated with an alias
 
-Microservice IDs can be separated with or without a space
-Microservice Aliases are case insensitive
-Aliases and IDs can be passed as parameters in any order
+-- Reference (ID:ALIAS) --
+1:DISC 2:CHIN 3:SVIN 4:PWTH 5:SCP 6:VID 7:VNW
+
+-- Input --
+Enter an IP address to assign it as host [100.68.125.170]
+Use the prefix 'host:' to set a named host [host:node-example]
+If no tests are specified, all will run by default
+Microservice IDs can be separated with or without a space [123 or 1 2 3]
+Microservice Aliases are case insensitive [DISC or disc]
+Use the modifier '^' or '!' to skip a test
+Host, Aliases, and IDs can be passed as parameters in any order
+[host:node-example CHIN svin 1 56 !2 ]
 
 Argument format examples:
 
->>> run_tests - Run All Tests
->>> run_tests 4 - Run Test With ID 4
->>> run_tests 7451725 - Run Tests With ID 7, 4, 1, 2, and 5
->>> run_tests SVIN - Run Test With ID 4
->>> run_tests DISC SCP CHIN - Run Tests with ID 1, 6, and 3
->>> run_tests 24 vNw 314 ScP - Run Tests with ID 2, 4, 7, 3, 1, and 6
+>>> run_tests - Run all tests
+>>> run_tests 4 - Run test with ID 4
+>>> run_tests 7451725 - Run tests with ID 7, 4, 1, 2, and 5
+>>> run_tests SVIN - Run test with ID 4
+>>> run_tests DISC SCP CHIN - Run tests with ID 1, 6, and 3
+>>> run_tests 24 vNw 314 ScP - Run tests with ID 2, 4, 7, 3, 1, and 6
+>>> run_tests ^345 - Run all tests except those with ID 3, 4, and 5
+>>> run_tests 100.68.125.170 5! - Run all tests on specified host except test 5
+>>> run_tests host:node-example 345 - Run tests with ID 3, 4, 5 on specified host
 
 :Copyright: (c) 2017 DELL Inc. or its subsidiaries.  All Rights Reserved.
 :License: Apache 2.0, see LICENSE for more details.
@@ -36,6 +54,7 @@ __copyright__ = 'Copyright 2017 DELL Inc.'
 
 import sys
 import unittest
+import re
 
 from discoveryTest import DiscoveryMicroserviceTest as disc
 from chassisInventoryTest import ChassisInventoryMicroserviceTest as chin
@@ -45,91 +64,129 @@ from scpTest import SCPMicroserviceTest as scp
 from virtualIdentityTest import VirtualIdentityTest as vid
 from virtualNetworkTest import VirtualNetworkTest as vnw
 
-# ----- ARGUMENT CONFIGURATION PROFILE -----
-##############################################
+# ------------ ARGUMENT CONFIGURATION PROFILE ------------
+##########################################################
+
+# Default Host
+HOST = "localhost"
 
 # Microservice ID
-ID = {
-    # Device Discovery
-    disc : 1,
-    # Chassis Inventory
-    chin : 2,
-    # Server Inventory
-    svin : 3,
-    # Power Thermal Monitoring
-    pwth : 4,
-    # Server Configuration Profile
-    scp : 5,
-    # Virtual Identity
-    vid : 6,
-    # Virtual Network
-    vnw : 7
+M_ID = {
+
+    '1' : disc,
+    '2' : chin,
+    '3' : svin,
+    '4' : pwth,
+    '5' : scp,
+    '6' : vid,
+    '7' : vnw
+
 }
+
+# Microservice ID Reversed
+M_ID_R = {m_id: mservice for mservice, m_id in M_ID.items()}
+
 # Microservice Alias
 ALIAS = {
-    'DISC' : ID[disc],
-    'CHIN' : ID[chin],
-    'SVIN' : ID[svin],
-    'PWTH' : ID[pwth],
-    'SCP' : ID[scp],
-    'VID' : ID[vid],
-    'VNW' : ID[vnw]
+
+    'DISC' : M_ID_R[disc],
+    'CHIN' : M_ID_R[chin],
+    'SVIN' : M_ID_R[svin],
+    'PWTH' : M_ID_R[pwth],
+    'SCP' : M_ID_R[scp],
+    'VID' : M_ID_R[vid],
+    'VNW' : M_ID_R[vnw]
+
 }
 
-##############################################
+##########################################################
 
-def _generate_test_keys(arguments):
+def _parse_arguments(arguments):
     "Parse data from arguments to determine which tests will be loaded"
-
-    # Default to returning keys for all tests
-    if not arguments:
-        return {val for val in ALIAS.values()}
-
-    test_keys = set()
-
-    def _add_key_from_id(number):
-        for digit in str(number):
-            key_num = int(digit)
-            if 1 <= key_num <= 8:
-                test_keys.add(key_num)
+    test_keys = set() # Store all keys for tests to run
+    remove_keys = set() # Store all keys for tests to skip
+    test_arguments = [] # Arguments to be parsed into keys
+    remove_arguments = [] # Arguments to be parsed into keys
+    host = HOST
+    if arguments:
+        # Parse out special arguments
+        for arg in arguments:
+            # Check for host IP address
+            if re.search("localhost|\\.", str(arg).lower()):
+                host = str(arg).lower()
+            # Check for named host
+            elif re.search("host:", str(arg).lower()):
+                host = re.sub("host:", "", str(arg).lower())
+            # Check for skip modifiers
+            elif re.search("[\\^\\!]", str(arg)):
+                remove_arguments.append(re.sub("[\\^\\!]", "", str(arg)))
             else:
-                raise ValueError('Invalid Microservice ID - {}'.format(key_num))
+                test_arguments.append(arg)
+    # Default run all tests
+    if not test_arguments:
+        test_arguments = {val for val in ALIAS.values()}
 
-    def _add_key_from_alias(key_alias):
-        key_alias = key_alias.upper()
-        if key_alias in ALIAS:
-            test_keys.add(ALIAS[key_alias])
-        else:
-            raise ValueError('Invalid Microservice Alias - {}'.format(key_alias))
+    def _add_arguments_to_set(set_arguments, target_set):
 
-    for arg in arguments:
-        if str(arg).isdecimal():
-            _add_key_from_id(arg)
-        elif arg.isalpha():
-            _add_key_from_alias(arg)
-        else:
-            raise ValueError('Invalid Argument - {}'.format(str(arg)))
+        def _add_key_from_m_id(m_id_string):
+            for m_id in str(m_id_string):
+                if m_id in M_ID:
+                    target_set.add(m_id)
+                else:
+                    raise ValueError('Invalid Microservice ID - {}'.format(m_id))
 
-    return test_keys
+        def _add_key_from_alias(alias):
+            alias = alias.upper()
+            if alias in ALIAS:
+                target_set.add(ALIAS[alias])
+            else:
+                raise ValueError('Invalid Microservice Alias - {}'.format(alias))
+
+        for arg in set_arguments:
+            if str(arg).upper() in ALIAS:
+                _add_key_from_alias(arg)
+            elif str(arg).isalnum():
+                _add_key_from_m_id(arg)
+            else:
+                raise ValueError('Invalid Argument - {}'.format(str(arg)))
+
+    _add_arguments_to_set(remove_arguments, remove_keys)
+    _add_arguments_to_set(test_arguments, test_keys)
+    return host, (test_keys - remove_keys)
 
 def _load_tests(test_keys):
     "Load tests into suite for each of the test keys passed in"
     test_suite = []
-    service_dict = {service_id: service for service, service_id in ID.items()}
     for key in test_keys:
-        test_suite.append(unittest.TestLoader().loadTestsFromTestCase(service_dict[key]))
+        test_suite.append(unittest.TestLoader().loadTestsFromTestCase(M_ID[key]))
     return unittest.TestSuite(test_suite)
 
 def run_tests(*args):
     "Run specified tests using arguments"
     arguments = [arg for arg in args]
-    # logger.info("Arguments - {}".format(arguments))
-    keys = _generate_test_keys(arguments)
-    # logger.info("Generated Keys - {}".format(keys))
+    # logger.info("Arguments: {}".format(arguments))
+    host, keys = _parse_arguments(arguments)
+    # logger.info("Parsed Host: {}".format(host))
+    # logger.info("Parsed Keys: {}".format(keys))
     test_suite = _load_tests(keys)
-    # logger.debug("Loaded Tests - {}".format(test_suite))
+    # logger.debug("Loaded Tests: {}".format(test_suite))
     unittest.TextTestRunner(verbosity=2).run(test_suite)
 
+def _parse_keys_tester():
+    test_cases = [[], [123], ['SCP'], ['chin', 2, 'DIsc'], [13, 'vnw', 455],
+                  ['^5'], ['host:node-wright'], ['!chin'], ['192.168.0.1'],
+                  ['localhost', 32, 'SCP', '^3', '!2', '100.0.0.255'],
+                  ['host:node-example', '5!'], ['^1237']]
+    print("\nBegin Testing\n\nID : ALIAS\n")
+    for alias in ALIAS:
+        print("{} : {}".format(ALIAS[alias], alias))
+    print("\n")
+    for index, case in enumerate(test_cases):
+        print("--Test {}--\n Args:{}".format(index, case))
+        host, keys = _parse_arguments(case)
+        print("Host: {}".format(host))
+        print("Parsed Keys: {}\n".format(keys))
 
 if __name__ == '__main__':
+    # _parse_keys_tester()
     run_tests(*sys.argv[1:])
