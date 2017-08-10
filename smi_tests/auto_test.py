74a65f1a3aa20241769e3b7df3f1202f195cc57a
Akash Kwatra <akashkw@gmail.com>
2017-08-09 23:16:22 -0500

    Documentation Updates

--- smi_tests/auto_test.py
+++ smi_tests/auto_test.py
@@ -12,22 +12,48 @@ Run specified microservice tests on specified host.
 Tests run in no particular order.
 
 -- Structure --
+Defaults are configured in "config.py"
 The default host is 'localhost'
+The default data directory is 'test_data'
+The default depth is 1
+
 Each microservice is assigned a 1 character ID
 Each microservice ID is associated with an alias
 
--- Reference (ID:ALIAS) --
-1:DISC 2:CHIN 3:SVIN 4:PWTH 5:SCP 6:VID 7:VNW 8:FWUP
+-- Reference (ID : ALIAS : MICROSERVICE) --
+1 : DISC : Device Discovery
+2 : CHIN : Chassis Inventory
+3 : SVIN : Server Inventory
+4 : PWTH : Power Thermal
+5 : SCP : Server Configuration Profile
+6 : VRID : Virtual Identity
+7 : VRNW : Virtual Network
+8 : FWUP : Firmware Update
+9 : OSDP : OS Deployment
 
 -- Input --
+HOST
+The address of the host running the microservices
 Enter an IP address to assign it as host [100.68.125.170]
 Use the prefix 'host:' to set a named host [host:node-example]
+
+DATA DIRECTORY
+The folder containing JSON defined test cases
 Enter a directory path to assign it as data directory [./test_data]
 Use the prefix 'data:' to set a data directory [data:test_data]
+
+DEPTH
+The extent to which error handling will be tested
+Higher depth takes longer to run
+    Depth 1 - Send Empty Data
+    Depth 2 - Iterate through data with bad values
+    Depth 3 - Generate all combinations of data with bad values
+Use the prefix 'depth:' to set depth 1-3 [depth:2]
+
 If no tests are specified, all will run by default
 Microservice IDs can be separated with or without a space [123 or 1 2 3]
 Microservice Aliases are case insensitive [DISC or disc]
-Use the modifier '^' or '!' to skip a test
+Use the modifier '^' or '!' to skip a microservice
 Host, Aliases, and IDs can be passed as parameters in any order
 [host:node-example CHIN svin 1 56 !2]
 
@@ -38,8 +64,8 @@ Argument format examples:
 >>> auto_test 4 - Run test with ID 4
 >>> auto_test 7451725 - Run tests with ID 7, 4, 1, 2, and 5
 >>> auto_test SVIN - Run test with ID 4
->>> auto_test DISC SCP CHIN - Run tests with ID 1, 6, and 3
->>> auto_test 24 vrNw 314 ScP - Run tests with ID 2, 4, 7, 3, 1, and 6
+>>> auto_test DISC SCP CHIN depth:3 - Run tests with ID 1, 6, and 3 with depth:3
+>>> auto_test 24 vrnw 314 Scp - Run tests with ID 2, 4, 7, 3, 1, and 6
 >>> auto_test ^345 - Run all tests except those with ID 3, 4, and 5
 >>> auto_test 100.68.125.170 ./test_data 5! - Run all tests from test_data on specified host except test 5
 >>> auto_test host:node-example 345 - Run tests with ID 3, 4, 5 on specified host
@@ -130,31 +156,6 @@ def run_tests(keys):
     test_suite = _load_tests(keys)
     LOG.debug("Loaded Tests: %s", test_suite)
     unittest.TextTestRunner(verbosity=2).run(test_suite)
-
-def parse_keys_tester():
-    """Unit test cases to make sure correct keys are parsed from given arguments"""
-    test_cases = [[], [123], ['SCP'], ['chin', 2, 'DIsc'], [13, 'vrnw', 455],
-                  ['^5'], ['host:node-wright'], ['!chin'], ['192.168.0.1'],
-                  ['localhost', 32, 'SCP', '^3', '!2', '100.0.0.255'],
-                  ['host:node-example', '5!'], ['^1237'], ['./test_data'],
-                  ['localhost', 32, 'SCP', '^3', '!2', './sample_data'],
-                  ['data:node-example', '5!', 'host:testing']]
-    print("\nBegin Testing\n\nID : ALIAS\n")
-    for alias in ALIAS:
-        print("{} : {}".format(ALIAS[alias], alias))
-    print("\n")
-    for index, case in enumerate(test_cases):
-        LOG.info("Running test %s", (index + 1))
-        print("\n--Test {}--\n Args:{}".format(index + 1, case))
-        try:
-            host, data, depth, keys = parse.auto_test_args(M_ID, ALIAS, *case)
-            print("Host: {}".format(host))
-            print("Data Directory: {}".format(data))
-            print("Depth: {}".format(depth))
-            print("Parsed Keys: {}".format(keys))
-        except Exception:
-            print("TEST FAILED")
-            LOG.exception("Test failed with args {}".format(case))
 
 if __name__ == '__main__':
     PARSED_HOST, PARSED_DATA, PARSED_DEPTH, KEYS = parse.auto_test_args(M_ID, ALIAS, *sys.argv[1:])
