8e8589ba53a8faf1668f9fa8aff8e27d29d308ad
Akash Kwatra <akashkw@gmail.com>
2017-08-10 21:54:57 -0500

    Disable tests with bugs

--- smi_tests/config.py
+++ smi_tests/config.py
@@ -6,7 +6,7 @@ Default configuration data for SMI-TEST-AUTOMATION
 #-------------- DEFAULT TEST CONFIGURATION ---------------
 ##########################################################
 
-HOST = '100.68.125.172'
+HOST = '100.68.123.45'
 
 DATA = 'test_data'
 
